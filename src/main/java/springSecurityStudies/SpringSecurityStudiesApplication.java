e8f3ee8b000e40fdb379e4d1c55678d62dda852d
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-10-14 16:38:31 -0300

    Feat: Início do projeto

---
+++ src/main/java/springSecurityStudies/SpringSecurityStudiesApplication.java
@@ -0,0 +1,13 @@
+package springSecurityStudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStudiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityStudiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springSecurityStudies/SpringSecurityStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package springSecurityStudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStudiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
