9dc1e128cdc9b6301b477961430b02c9ca981c87
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-10-14 19:24:07 -0300

    Feat: Criação dos DTOs de response

---
+++ src/main/java/springSecurityStudies/dto/response/LoginResponse.java
@@ -0,0 +1,6 @@
+package springSecurityStudies.dto.response;
+
+public record LoginResponse(
+        String token
+) {
+}
---
+++ src/main/java/springSecurityStudies/dto/response/RegisterUserResponse.java
@@ -0,0 +1,7 @@
+package springSecurityStudies.dto.response;
+
+public record RegisterUserResponse(
+        String name,
+        String email
+) {
+}
