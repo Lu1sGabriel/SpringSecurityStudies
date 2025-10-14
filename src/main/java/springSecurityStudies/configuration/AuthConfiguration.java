7c734b0feb5c6e3b8f004ef8d3622df1004f17b2
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-10-14 19:47:14 -0300

    Feat: Criação da classe `AuthConfiguration`

---
+++ src/main/java/springSecurityStudies/configuration/AuthConfiguration.java
@@ -0,0 +1,21 @@
+package springSecurityStudies.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import springSecurityStudies.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class AuthConfiguration implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+}
