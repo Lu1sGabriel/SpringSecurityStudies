3778826520aa2509b3c7aed5f256f40ae47ae7d6
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-10-14 19:32:36 -0300

    Feat: Criação da classe de configuração `SecurityConfiguration`.
    - Configura as requisições que precisam ou não das autorizações.
    - Cria o  `Bean` do `securityFilterChain`
    - Cria o `Bean` do `passwordEncoder`
    - Cria o `Bean` do `authenticationManager`

---
+++ src/main/java/springSecurityStudies/configuration/SecurityConfiguration.java
@@ -0,0 +1,55 @@
+package springSecurityStudies.configuration;
+
+import jakarta.servlet.DispatcherType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    private final SecurityFilter securityFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> cors.configure(httpSecurity))
+                .sessionManagement(session -> session.sessionCreationPolicy(
+                        //  Não haverá sessão no servidor. Ideal para APIs que usam JWT ou tokens.
+                        //  Pois cada requisição vai trazer a sua autenticação, sem depender da sessão.
+                        SessionCreationPolicy.STATELESS
+                ))
+                .authorizeHttpRequests(authorize -> authorize
+                        .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
