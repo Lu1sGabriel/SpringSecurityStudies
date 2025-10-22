5ba32f3d9c5f8746270f7c6cd1d38cd92627e31c
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-10-22 16:43:30 -0300

    Feat: Adição da Role no dto de criação do usuário

--- src/main/java/springSecurityStudies/dto/request/RegisterUserRequest.java
+++ src/main/java/springSecurityStudies/dto/request/RegisterUserRequest.java
@@ -1,10 +1,21 @@
 package springSecurityStudies.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import springSecurityStudies.entity.Role;
 
 public record RegisterUserRequest(
-        @NotEmpty(message = "O nome é obrigatório.") String name,
-        @NotEmpty(message = "O email é obrigatório.") String email,
-        @NotEmpty(message = "A senha é obrigatória.") String password
+
+        @NotEmpty(message = "O nome é obrigatório.")
+        String name,
+
+        @NotEmpty(message = "O email é obrigatório.")
+        @Email
+        String email,
+
+        @NotEmpty(message = "A senha é obrigatória.")
+        String password,
+
+        Role role
 ) {
 }
