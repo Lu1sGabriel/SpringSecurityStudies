f766e819bb45fdfbe7433b68fee72cdf0d137731
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-10-22 16:45:45 -0300

    Feat: Passando dentro `UsernamePasswordAuthenticationToken` as suas autorizações, que no caso, são as roles.

--- src/main/java/springSecurityStudies/configuration/SecurityFilter.java
+++ src/main/java/springSecurityStudies/configuration/SecurityFilter.java
@@ -7,11 +7,13 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
 
 @Component
@@ -27,7 +29,13 @@ public class SecurityFilter extends OncePerRequestFilter {
             Optional<JWTUserData> userDataOptional = tokenConfiguration.validateToken(token);
             if (userDataOptional.isPresent()) {
                 JWTUserData userData = userDataOptional.get();
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userData, null, null);
+
+                List<SimpleGrantedAuthority> authorities = userData.roles()
+                        .stream()
+                        .map(SimpleGrantedAuthority::new)
+                        .toList();
+
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userData, null, authorities);
                 SecurityContextHolder.getContext().setAuthentication(authentication);
             }
             filterChain.doFilter(request, response);
